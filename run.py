fa0628102a597ed61523c1e74fb9d7f32aee1934
bcloutier412 <brandoncloutier412@gmail.com>
2024-07-15 14:29:03 -0700

    finished writing 2d array

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 print("hello")
 
 w, h = 6, 8
-strandsArray = [[0 for x in range(w)] for y in range(h)] 
+# strandsArray = [[0 for x in range(w)] for y in range(h)]
+strandsArray = [['e', 'h' ,'t', 'o', 'n', 'e'],
+                ['s', 'w', 'r', 't', 's', 'c'],
+                ['t', 'r', 'r', 'f', 'u', 'i'],
+                ['e', 'n', 's', 'e', 't', 'r'],
+                ['s', 'e', 'i', 'v', 'n', 'u'],
+                ['t', 'i', 't', 'i', 'b', 'd'],
+                ['e', 'n', 'w', 'o', 'r', 'u'],
+                ['m', 'p', 'l', 'e', 'e', 'k']] 
 
 
 
