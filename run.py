b34bbf6cca9871ab5ead90ae116bbe7efc891468
Calvin Hunter <calhunte@ucsc.edu>
2024-07-15 14:19:09 -0700

    starter run.py

---
+++ run.py
@@ -0,0 +1,24 @@
+print("hello")
+
+w, h = 6, 8
+strandsArray = [[0 for x in range(w)] for y in range(h)] 
+
+
+
+def checkRight(x, y):
+    return True or False
+
+def checkLeft(x, y):
+    return True or False
+
+def checkUp(x, y):
+    return True or False
+
+def checkDown(x, y):
+    return True or False
+
+def checkRightDiag(x,y):
+    return True or False
+
+def checkLeftDiag(x, y):
+    return True or False
