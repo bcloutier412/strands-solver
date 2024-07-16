f1866f9933a606c3b43e56d5dde87961988f9ab7
bcloutier412 <brandoncloutier412@gmail.com>
2024-07-16 12:29:33 -0700

    Separated AVLTree class into module and created Word Dictionary Module

---
+++ DictionaryAVLTree.py
@@ -0,0 +1,27 @@
+from AVLTree import Node, AVLTree
+import sys
+
+# Constants
+MINIMUM_WL = 4
+MAXIMUM_WL = 15
+
+# Function to simulate a dynamic print
+def dynamic_print(text):
+    sys.stdout.write('\r' + text)
+    sys.stdout.flush()
+
+dictionaryAVLTree = AVLTree()
+
+print("[Creating Dict AVL Tree]")
+
+with open("words_alpha.txt", "r") as file:
+  counter = 0
+  for word in file:
+    currWord = word.strip()
+    if len(currWord) >= MINIMUM_WL and len(currWord) <= MAXIMUM_WL:
+      dictionaryAVLTree.insert_value(currWord)
+      counter += 1
+      dynamic_print(f"{counter} words added")
+
+print()
+print("[Finished Dict AVL Tree]")
