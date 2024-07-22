3175f825930d6eb21c09f7e6a83fe9a1145b2f7e
bcloutier412 <brandoncloutier412@gmail.com>
2024-07-22 16:22:52 -0700

    added diagonal checks so they it wont cross diagonal already picked letters

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ WIDTH, HEIGHT = 6, 8
 outputFile = open("strands_words.txt", "w")
 
 # strandsArray = [[0 for x in range(w)] for y in range(h)]
-strandsArray = [['e', 'i' ,'f', 'n', 'a', 'w'],
-                ['d', 'l', 'k', 'r', 'e', 's'],
-                ['b', 'e', 's', 'r', 'n', 'b'],
-                ['h', 'p', 't', 'y', 'i', 'g'],
-                ['t', 'o', 'a', 't', 'x', 's'],
-                ['n', 'h', 'e', 'c', 'i', 'e'],
-                ['u', 'o', 't', 'n', 'd', 'g'],
-                ['n', 'e', 's', 'i', 'r', 'b']]
+strandsArray = [['a', 'w' ,'a', 'b', 'o', 'a'],
+                ['l', 'd', 'n', 't', 'e', 'r'],
+                ['o', 'o', 'g', 'e', 'l', 'd'],
+                ['i', 'd', 'c', 'r', 'd', 'k'],
+                ['n', 'r', 'p', 'a', 'd', 'a'],
+                ['g', 'y', 'a', 'k', 'a', 'y'],
+                ['h', 'd', 'f', 'n', 'o', 'e'],
+                ['o', 'r', 'y', 't', 'a', 'c']]
 
 # recursive function
 # @Params
@@ -39,19 +39,35 @@ def searchLetter(currentString, row, col, usedLetters):
             outputFile.write(currentString + '\n')
 
     # all possible directions to check
-    directions = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    cardinalDirections = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    diagonalDirections = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
+    checkDiagonalDirections = [((-1, 0), (0, 1)), ((0, 1), (1, 0)), ((1, 0), (0, -1)), ((0, -1), (-1, 0))]
 
-    for direction in directions:
+    for direction in cardinalDirections:
         newRow, newCol = row + direction[0], col + direction[1]
-        searchNextLetter(currentString, newRow, newCol, usedLetters)
+        searchNextCardinalLetter(currentString, newRow, newCol, usedLetters)
+    
+    for index, direction in enumerate(diagonalDirections):
+        newRow, newCol = row + direction[0], col + direction[1]
+        checkRow1, checkCol1 = row + checkDiagonalDirections[index][0][0], col + checkDiagonalDirections[index][0][1]
+        checkRow2, checkCol2 = row + checkDiagonalDirections[index][1][0], col + checkDiagonalDirections[index][1][1]
+        searchNextDiagonalLetter(currentString, newRow, newCol, usedLetters, checkRow1, checkCol1, checkRow2, checkCol2)
 
-def searchNextLetter(currentString, row, col, usedLetters):
+
+def searchNextCardinalLetter(currentString, row, col, usedLetters):
     # Makes sure it is a valid element in the 2D matrix and also it hasnt been used yet
     if isInBounds(row, col) and getId(row, col) not in usedLetters:
         usedLetters.add(getId(row, col))
         searchLetter(currentString + strandsArray[row][col], row, col, usedLetters)
         usedLetters.remove(getId(row, col))
 
+def searchNextDiagonalLetter(currentString, row, col, usedLetters, checkRow1, checkCol1, checkRow2, checkCol2):
+    # Makes sure it is a valid element in the 2D matrix and also hasnt been used yet and also makes sure that it wont be searching a letter that goes diagonal between two picked letters
+    if isInBounds(row, col) and getId(row, col) not in usedLetters and not (getId(checkRow1, checkCol1) in usedLetters and getId(checkRow2, checkCol2) in usedLetters):
+        usedLetters.add(getId(row, col))
+        searchLetter(currentString + strandsArray[row][col], row, col, usedLetters)
+        usedLetters.remove(getId(row, col))
+    
 def isInBounds(row, col):
     if (row < 0 or row >= len(strandsArray)) or (col < 0 or col >= len(strandsArray[0])):
         return False
