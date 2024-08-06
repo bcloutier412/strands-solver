e0dbb68901d252c583b901aeb05260f914d344ad
bcloutier412 <brandoncloutier412@gmail.com>
2024-08-06 13:21:11 -0700

    formatted python code in src

--- src/py/MatrixWordFinderClass.py
+++ src/py/MatrixWordFinderClass.py
@@ -1,22 +1,33 @@
 from AVLTree import Node, AVLTree
 import sys
 
-# Constants
-MINIMUM_WL = 4
-MAXIMUM_WL = 15
 
 # Function to simulate a dynamic print
 def dynamic_print(text):
-    sys.stdout.write('\r' + text)
+    sys.stdout.write("\r" + text)
     sys.stdout.flush()
 
+
 class MatrixWordFinder:
-  def __init__(self, gameboard, total_words_to_complete_matrix, dictionary_avl_tree):
-    self.WIDTH = 6
-    self.HEIGHT = 8
-    self.gameboard = gameboard
-    self.total_words_to_complete_matrix = total_words_to_complete_matrix
-    self.dictionary_avl_tree = dictionary_avl_tree
-    
-  def search_letter():
-     return
+    def __init__(
+        self,
+        WIDTH,
+        HEIGHT,
+        MINIMUM_WL,
+        MAXIMUM_WL,
+        gameboard,
+        total_words_to_complete_matrix,
+        possible_words,
+        dictionary_avl_tree,
+    ):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.MINIMUM_WL = MINIMUM_WL
+        self.MAXIMUM_WL = MAXIMUM_WL
+        self.gameboard = gameboard
+        self.total_words_to_complete_matrix = total_words_to_complete_matrix
+        self.dictionary_avl_tree = dictionary_avl_tree
+        self.possible_words = possible_words
+
+    def search_letter():
+        return
--- src/py/find_possible_words.py
+++ src/py/find_possible_words.py
@@ -1,9 +1,30 @@
 from MatrixWordFinderClass import MatrixWordFinder
 from DictionaryAVLTree import dictionary_avl_tree
+import json
+
 
 def main():
-  # Open the runtime_data file and import the 2D matrix and the total_words
-  matrix_word_finder = MatrixWordFinder([], 0, dictionary_avl_tree)
-  return 0
+    # Open the runtime_data file and import the 2D matrix and the total_words
+    # Loading JSON file
+    with open("data/runtime_data.json", "r") as file:
+        runtime_data = json.load(file)
+
+    matrix_word_finder = MatrixWordFinder(
+        runtime_data["WIDTH"],
+        runtime_data["HEIGHT"],
+        runtime_data["MINIMUM_WL"],
+        runtime_data["MAXIMUM_WL"],
+        runtime_data["gameboard"],
+        runtime_data["total_words_to_complete_matrix"],
+        runtime_data["possible_words"],
+        dictionary_avl_tree
+    )
+
+    # Updating JSON file
+    with open("data/runtime_data.json", "w") as file:
+        json.dump(runtime_data, file, indent=4, separators=(",", ": "))
+
+    return 0
+
 
 main()
