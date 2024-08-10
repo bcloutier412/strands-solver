79adf0580808bb0b9239bd18e394085aec528eb2
bcloutier412 <brandoncloutier412@gmail.com>
2024-08-10 12:15:45 -0700

    pushing changes

--- src/py/DictionaryAVLTree.py
+++ src/py/DictionaryAVLTree.py
@@ -1,10 +1,10 @@
 from AVLTree import Node, AVLTree
 from utils import get_relative_file_path
 import sys
+import json
 
-# Constants
-MINIMUM_WL = 4
-MAXIMUM_WL = 15
+with open(get_relative_file_path("../../data/runtime_data.json"), "r") as file:
+        runtime_data = json.load(file)
 
 # Function to simulate a dynamic print
 def dynamic_print(text):
@@ -19,7 +19,7 @@ with open(get_relative_file_path("../text_files/words_alpha.txt"), "r") as file:
   counter = 0
   for word in file:
     curr_word = word.strip()
-    if len(curr_word) >= MINIMUM_WL and len(curr_word) <= MAXIMUM_WL:
+    if len(curr_word) >= runtime_data["MINIMUM_WL"] and len(curr_word) <= runtime_data["MAXIMUM_WL"]:
       dictionary_avl_tree.insert_value(curr_word)
       counter += 1
       dynamic_print(f"{counter} words added")
